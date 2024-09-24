d80265307a3f9444bdfa4c12dfaa946055e2c0bb
vovanhau123 <levanhau16112002@gmail.com>
2024-09-24 14:22:25 +0700

    update

--- host-app/src/remotes.d.ts
+++
@@ -1,9 +0,0 @@
-
-// declare module 'walletApp/WalletComponent' {
-//     const WalletComponent: React.ComponentType;
-//     export default WalletComponent;
-//   }
-declare module 'remoteApp/Config' {
-    const RemoteComponent: React.ComponentType<{name: string}>;
-    export default RemoteComponent;
-  }
